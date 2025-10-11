4ca1646f6887e1c2518052419f9ef8de02d6e82d
Phong Nguyen <24020269@vnu.edu.vn>
2025-10-11 13:30:00 +0700

    feat: add velocity management system to MovableObject

--- src/main/java/com/knightquest/arkanoid/model/MovableObject.java
+++ src/main/java/com/knightquest/arkanoid/model/MovableObject.java
@@ -19,10 +19,30 @@ public abstract class MovableObject extends GameObject {
     }
 
     /**
-     * Setter.
+     * Velocity setters and getters.
      */
     public void setVelocity(double dx, double dy) {
         this.dx = dx;
         this.dy = dy;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public double getDy() {
+        return dy;
+    }
+
+    public void setDx(double dx) {
+        this.dx = dx;
+    }
+
+    public void setDy(double dy) {
+        this.dy = dy;
+    }
+
+    public double getSpeed() {
+        return Math.sqrt(dx * dx + dy * dy);
     }
 }
