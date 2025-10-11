116cd35efe5cabb53693c91023568666c5768b7c
Phong Nguyen <24020269@vnu.edu.vn>
2025-10-11 13:31:25 +0700

    feat: improve ball physics for smoother gameplay

--- src/main/java/com/knightquest/arkanoid/model/Ball.java
+++ src/main/java/com/knightquest/arkanoid/model/Ball.java
@@ -1,8 +1,11 @@
 package com.knightquest.arkanoid.model;
 
+import static com.knightquest.arkanoid.util.Constants.BALL_RADIUS;
+import static com.knightquest.arkanoid.util.Constants.BALL_SPEED;
+import static com.knightquest.arkanoid.util.Constants.SCREEN_HEIGHT;
+
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-import static com.knightquest.arkanoid.util.Constants.*;
 
 public class Ball extends MovableObject {
     private double radius;
@@ -17,17 +20,21 @@ public class Ball extends MovableObject {
     }
 
     /**
-     * Update location.
+     * Update location with boundary checking moved to CollisionHandler.
      */
     @Override
     public void update(double deltaTime) {
         move(deltaTime);
-        if (x <= 0 || x + width >= SCREEN_WIDTH) {
-            dx = -dx;
-        }
-        if (y <= 0) {
-            dy = -dy;
-        }
+
+        // Clamp velocity to prevent excessive speeds
+        double maxSpeed = BALL_SPEED * 2; // Allow up to 2x normal speed
+        if (Math.abs(getDx()) > maxSpeed) setDx(Math.signum(getDx()) * maxSpeed);
+        if (Math.abs(getDy()) > maxSpeed) setDy(Math.signum(getDy()) * maxSpeed);
+
+        // Ensure minimum speed to prevent ball getting stuck
+        double minSpeed = BALL_SPEED * 0.3;
+        if (Math.abs(getDx()) < minSpeed) setDx(Math.signum(getDx()) * minSpeed);
+        if (Math.abs(getDy()) < minSpeed) setDy(Math.signum(getDy()) * minSpeed);
     }
 
     @Override
@@ -37,10 +44,11 @@ public class Ball extends MovableObject {
     }
 
     public void bounceVertical() {
-        dy = -dy;
+        setDy(-getDy());
     }
+
     public void bounceHorizontal() {
-        dx = -dx;
+        setDx(-getDx());
     }
 
     public boolean isFallenOff() {
