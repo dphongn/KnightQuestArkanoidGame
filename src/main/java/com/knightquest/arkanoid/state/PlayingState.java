06cc196c014a90bb28c445fa8e35b109e4a347b6
Phong Nguyen <24020269@vnu.edu.vn>
2025-10-21 15:51:28 +0700

    Fix: correct syntax in PlayingState

--- src/main/java/com/knightquest/arkanoid/state/PlayingState.java
+++ src/main/java/com/knightquest/arkanoid/state/PlayingState.java
@@ -62,7 +62,7 @@ public class PlayingState extends GameState {
 
         //Handle pause
         if (keyPressed && keyCode == KeyCode.ESCAPE) {
-            changeState(new PauseState(gameManager));
+            changeState(new PausedState(gameManager));
             return;
         }
 
@@ -85,7 +85,7 @@ public class PlayingState extends GameState {
     public void render(GraphicsContext gc) {
         // Clear screen
         gc.setFill(Color.rgb(15 ,15 ,25));
-        gc.fillRect(0. 0. gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+        gc.fillRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
 
         //Draw bricks
         for (Brick brick : gameManager.getBricks()) {
