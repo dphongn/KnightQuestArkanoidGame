42ef00958877ad3b683f04720e0f8b84ec43be72
Phong Nguyen <24020269@vnu.edu.vn>
2025-11-01 23:12:01 +0700

    feat(powerup): Add GunPaddlePowerUp with shooting mechanics

--- src/main/java/com/knightquest/arkanoid/model/powerup/GunPaddlePowerUp.java
+++ src/main/java/com/knightquest/arkanoid/model/powerup/GunPaddlePowerUp.java
@@ -4,32 +4,36 @@ import com.knightquest.arkanoid.model.Paddle;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
-public class LaserPaddlePowerUp extends PowerUp {
-    private static final double LASER_COOLDOWN = 0.5;
+public class GunPaddlePowerUp extends PowerUp {
+    private static final double DURATION = 10.0; // 10 seconds
+    private static final double GUN_COOLDOWN = 0.3; // 0.3s between shots
 
-    public LaserPaddlePowerUp(double x, double y) {
-        super(x, y, PowerUpType.LASER_PADDLE, PowerUpType.LASER_PADDLE.getDefaultDuration());
+    public GunPaddlePowerUp(double x, double y) {
+        super(x, y, PowerUpType.GUN_PADDLE, DURATION);
     }
 
     @Override
     public void apply(Paddle paddle) {
-        paddle.setCanShootLaser(true);
-        paddle.setLaserCooldown(LASER_COOLDOWN);
+        System.out.println("🔫 Gun Paddle power-up APPLIED!");
+        paddle.setCanShootGun(true);
+        paddle.setGunCooldown(GUN_COOLDOWN);
+        System.out.println("   Can shoot: " + paddle.canShootGun());
     }
 
     @Override
     public void remove(Paddle paddle) {
-        paddle.setCanShootLaser(false);
+        System.out.println("🔫 Gun Paddle power-up REMOVED!");
+        paddle.setCanShootGun(false);
     }
 
     @Override
     protected void renderIcon(GraphicsContext gc) {
         gc.setStroke(Color.WHITE);
         gc.setLineWidth(2);
-        //Draw laser beams
+        // Draw gun barrels (laser beams pointing up)
         gc.strokeLine(x + 10, y + 20, x + 10, y + 5);
         gc.strokeLine(x + 20, y + 20, x + 20, y + 5);
-        //Draw paddle base
+        // Draw paddle base
         gc.setFill(Color.WHITE);
         gc.fillRect(x + 7, y + 22, 16, 4);
     }
